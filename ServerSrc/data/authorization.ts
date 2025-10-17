ef47b260ee5df6caa7be3f6373595d4ae6ed5ef0
Sara Serti <sara.serti@outlook.com>
2025-10-17 09:42:34 +0200

    types

--- ServerSrc/data/authorization.ts
+++ ServerSrc/data/authorization.ts
@@ -1,3 +1,18 @@
 import jwt from 'jsonwebtoken'
 
-const jwtSecret: string = process.env.JWT_SECRET || ''
+const jwtSecret: string = process.env.MY_JWT_SECRET || ''
+
+function token(userId: string): string {
+	// Tiden sedan 1970-01-01 i sekunder
+	const now = Math.floor(Date.now() / 1000)
+
+	// En kvart
+	const defaultExpiration: number = now + 10 * 60
+	return jwt.sign({
+		userId: userId,
+		// TODO: lägg till accessLevel för att göra det möjligt för admin att ta bort alla
+		exp: defaultExpiration
+	}, jwtSecret)
+}
+
+export { token }
--- ServerSrc/data/types.ts
+++ ServerSrc/data/types.ts
@@ -5,3 +5,23 @@ export interface UserItem {
 	password: string;
 	accessLevel: string;
 }
+export interface MessageResp {
+	message: string;
+}
+export interface LoginResp {
+	success: boolean;
+	token?: string;
+}
+export interface SigninBody {
+	username: string;
+	password: string;
+}
+export interface User {
+	userId: string;
+	username: string;
+	password: string;  
+}
+export interface Payload {
+	username: string;
+	expiration: number;  
+}
